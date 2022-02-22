78b739759c67a4261818e47abc8fce00eb0dc20f
jpena208 <49579138+jpena208@users.noreply.github.com>
2022-02-21 20:01:27 -0600

    Updated skills section with new icons and information, as well as interests. Contact section now has a contact form

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+http = require('http');
+fs = require('fs');
+path = require('path');
+
+// If port is set in environment variable use that port
+// if not, use port 5000
+const PORT = process.env.PORT || 5000
+
+app.use(express.static(path.join('C:/Users/jpena208/Documents/GitHub/josepena-website')));
+
+app.get("/", function (req, res) {
+    res.sendFile("C:/Users/jpena208/Documents/GitHub/josepena-website/index.html");
+});
+
+app.listen(PORT, function () {
+    console.log("Server started...")
+});
