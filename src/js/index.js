54a8c30393a731260aa4596df6c7202695108cea
jpena208 <49579138+jpena208@users.noreply.github.com>
2022-04-12 01:25:59 -0500

    Added new project cards and animations, preparing grid in experience to add more

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,52 @@
-// Adds animated class to icons on hover of paragraphs
-$(".icon-container").hover(function(){
-    // Adds fade animation to icons
-    let icon = $(this).find(".icon-animate");
-    $(icon).addClass("fa-fade");
-});
+/* Media Queries
+-------------------------------------------------- */
+// media query
+function mediaQuery() {
+  var mediaQuery = window.matchMedia("(max-width: 768px)");
+  mediaQuery.addEventListener(function (e) {
+    if (e.matches) {
+      // console.log('mobile');
+    } else {
+      // console.log('desktop');
+    }
+  });
+}
 
-// Removes animated class from icons on mouse exit
-$(".icon-container").mouseleave(function(){
+/* Animations
+-------------------------------------------------- */
+function addAnimatedClass() {
+  // Adds animated class to cards when mouse enters card
+  $(".project").hover(
+    function () {
+      $(this).find(".card-img").addClass("img-animation");
 
-    // Remove fade animation from icons
-    let icon = $(this).find(".icon-animate");
-    $(icon).removeClass("fa-fade");
-});
+      $(this).find(".circle").addClass("circle-animation");
+      $(this).find(".circle").removeClass("visually-hidden");
+      setTimeout(() => {
+        $(this).find(".card-title").removeClass("visually-hidden");
+      }, 500);
+    },
+    function () {
+      $(this).find(".card-img").removeClass("img-animation");
+
+      $(this).find(".circle").removeClass("circle-animation");
+      setTimeout(() => {
+        $(this).find(".circle").addClass("visually-hidden");
+      }, 200);
+    }
+  );
+
+  // Adds fade animation to icons when mouse enters icon container
+  $(".icon-container").hover(
+    function () {
+      $(this).find(".icon-animate").addClass("fa-fade");
+    },
+    function () {
+      $(this).find(".icon-animate").removeClass("fa-fade");
+    }
+  );
+}
+
+/* Call Functions
+-------------------------------------------------- */
+addAnimatedClass();
