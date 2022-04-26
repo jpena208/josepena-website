a8916ea2095e1f709429b465e6d97197457d5902
jpena208 <49579138+jpena208@users.noreply.github.com>
2022-04-26 00:27:51 -0500

    Added Anime JS to package, animated bits of the about section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 /* Media Queries
 -------------------------------------------------- */
-// media query
+/**
+ * @description: Media queries for responsiveness
+ */
 function mediaQuery() {
   var mediaQuery = window.matchMedia("(max-width: 768px)");
   mediaQuery.addEventListener(function (e) {
@@ -14,6 +16,9 @@ function mediaQuery() {
 
 /* Animations
 -------------------------------------------------- */
+/**
+ * @description: Adds css classes to elements to trigger animations
+ */
 function addAnimatedClass() {
   // Adds animated class to cards when mouse enters card
   $(".project").hover(
@@ -46,6 +51,83 @@ function addAnimatedClass() {
   );
 }
 
+/**
+ * @description: Adds Anime JS animation for about page on it's own timeline
+ */
+function addAboutAnime() {
+  // Wrap every letter in a span
+  var textWrapper = document.querySelector(".about-t1-2");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+  var textWrapper = document.querySelector(".about-t1-3");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+  var textWrapper = document.querySelector(".about-t1-4");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+  anime
+    .timeline()
+    .add({
+      targets: ".about-t1-1 .word",
+      scale: [14, 1],
+      opacity: [0, 1],
+      easing: "easeOutCirc",
+      duration: 600,
+      delay: (el, i) => 800 * i,
+    })
+    .add({
+      targets: ".about-t1-2 .letter",
+      scale: [4, 1],
+      opacity: [0, 1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 500,
+      delay: (el, i) => 70 * i,
+    })
+    .add({
+      targets: ".about-t1-3 .letter",
+      scale: [4, 1],
+      opacity: [0, 1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 500,
+      delay: (el, i) => 70 * i,
+    })
+    .add(
+      {
+        targets: ".about-t1-4 .letter",
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 500,
+        delay: (el, i) => 70 * i,
+      },
+      "-=400"
+    )
+    .add({
+      targets: ".about-t1-5",
+      opacity: [0, 1],
+      duration: 500,
+      easing: "linear",
+    })
+    .add({
+      targets: ".about-t1-6",
+      opacity: [0, 1],
+      translateY: [100, 0],
+      duration: 1700,
+      easing: "easeOutBounce",
+      delay: 100,
+    });
+}
+
 /* Call Functions
 -------------------------------------------------- */
 addAnimatedClass();
+addAboutAnime();
